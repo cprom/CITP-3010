98c74df0b1ce78d860d25e991bf2e0c8f329a702
Chen Prom <cprom@Chens-MBP.lan>
2023-09-10 21:53:36 -0500

    initial commit

---
+++ CProm_Lab1/CProm_Lab1.cs
@@ -0,0 +1,50 @@
+﻿
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Name: Chen Prom 
+// Date: 9/9/2023
+// Class: CITP-3010
+// Semester: FALL 2023
+// Instructor’s Name:NICOLAS GUTIERREZ
+//
+// Program Title: CProm_Lab1
+// Program Description: A console program to print out my name and what lab number using Console.WriteLine().
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class CProm_Lab1
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Chen Prom";
+            int labNumber = 1;
+
+            Console.WriteLine("Hello World!");     
+
+            Console.WriteLine($"My name is {name}.");
+            Console.WriteLine($"This    is     Lab {labNumber}.");
+                                                                                
+            Console.Write("\n\n");                  
+
+            Console.WriteLine("  CCCCCCCCC    PPPPPPPPPP     ");      
+            Console.WriteLine(" CCCCCCCC     PPPPPPPPPPPP    ");      
+            Console.WriteLine("CCC           PPP       PPP   ");
+            Console.WriteLine("CC            PPP       PPP   ");
+            Console.WriteLine("CC            PPPPPPPPPPPP    ");
+            Console.WriteLine("CC            PPPPPPPPPP      ");
+            Console.WriteLine("CCC           PPP             ");
+            Console.WriteLine(" CCCCCCCC     PPP             ");
+            Console.WriteLine("  CCCCCCCC    PPP             ");
+
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");      // This line prints the words "Hello World!" to the screen and includes a newline.
+               
+            Console.WriteLine("{0} {1} {2}", "Programming", "can be", "fun!");  // This line prints the sentence "Programming can be fun!"
+                                                                                // It uses format items, labeled {#}, to build more complicated strings.
+
+            Console.Write("\n\n");                  // Console.Write() does not include a newline automatically. We can use the escape character '\n'
+                                                    // to include new lines manually.
+
+            Console.WriteLine("  CCCCCCC    ###    ###     ");      // These lines make use of extra whitespace, which is NOT ignored inside of a string
+            Console.WriteLine(" CCCCCCC     ###    ###     ");      // to draw the C# shape you see here.
+            Console.WriteLine("CCC       ################  ");
+            Console.WriteLine("CC        ################  ");
+            Console.WriteLine("CC           ###    ###     ");
+            Console.WriteLine("CC        ################  ");
+            Console.WriteLine("CCC       ################  ");
+            Console.WriteLine(" CCCCCC      ###    ###     ");
+            Console.WriteLine("  CCCCCC     ###    ###     ");
+
+        }
+    }
+}
